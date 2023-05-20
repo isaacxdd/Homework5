c277f73af3985a9b0477976e740af2a501af78f8
Isaac Santacruz <Isaacsantacruz73@gmail.com>
2023-05-20 00:04:02 -0400

    Commit 4 - I realize I can make things that are ever increasingly more complex with greater ease. Adding order is always more effort than causing chaos

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 $(() => {
   
-    let $container = $('#container');
+  let $container = $('#container');
     
    
     $('#container').append($h1= $('<h1>').text('Hogwarts'))
@@ -19,6 +19,21 @@ $(() => {
     $unorderedList.append($('<li>').text('"Bertie Bott\'s Every Flavor [Jelly] Beans"'));
   
     $container.append($unorderedList);
-    
-  });
+
+    $('#container').append($h5= $('<h5>').text('Spring 2017'))
+
+    let $table = $('<table>');
+    let $thead = $('<thead>');
+    $thead.append($('<tr>').append($('<th>').text('Day'), $('<th>').text('Classes')));
+    $table.append($thead);
   
+    let $tbody = $('<tbody>');
+    $tbody.append($('<tr>').append($('<td>').text('Monday'), $('<td>').text('History of Magic, Charms, Potions, Transfiguration, Defense Against the Dark Arts, Quidditch practice')));
+    $tbody.append($('<tr>').append($('<td>').text('Tuesday'), $('<td>').text('Herbology, Divination, History of Magic, Charms, Potions, Transfiguration')));
+    $tbody.append($('<tr>').append($('<td>').text('Wednesday'), $('<td>').text('Charms, Potions, Transfiguration, Defense Against the Dark Arts, Quidditch practice')));
+    $tbody.append($('<tr>').append($('<td>').text('Thrusday'), $('<td>').text('Charms, Potions, Transfiguration, Defense Against the Dark Arts, Quidditch practice')));
+    $tbody.append($('<tr>').append($('<td>').text('Friday'), $('<td>').text('Quidditch practice,  Potions, Transfiguration')));
+    $table.append($tbody);
+  
+    $container.append($table);
+  });
